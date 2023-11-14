e3f57edb64b947066420e1db653fe91fecc29cf5
Mishal12g <mishal12g@gmail.com>
2023-11-14 16:50:42 +0300

    refactor: added person repository implimentation.

---
+++ lib/feature/data/repositories/person_repository_impl.dart
@@ -0,0 +1,54 @@
+import 'package:dartz/dartz.dart';
+import 'package:rick_and_morty/core/error/exception.dart';
+import 'package:rick_and_morty/core/error/failure.dart';
+import 'package:rick_and_morty/core/platform/network_info.dart';
+import 'package:rick_and_morty/feature/data/dataSources/person_local_data_cource.dart';
+import 'package:rick_and_morty/feature/data/dataSources/person_remote_data_source.dart';
+import 'package:rick_and_morty/feature/data/models/person_model.dart';
+import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+import 'package:rick_and_morty/feature/domain/repositories/person_repository.dart';
+
+class PersonRepositoryImpl implements PersonRepository {
+  final PersonRemoteDataSource remoteDataSource;
+  final PersonLocalDataSoutce localDataSoutce;
+  final NetworkInfo networkInfo;
+
+  PersonRepositoryImpl(
+      {required this.remoteDataSource,
+      required this.localDataSoutce,
+      required this.networkInfo});
+
+  @override
+  Future<Either<Failure, List<PersonEntity>>> getAllPerson(int page) async {
+    return await _getPersons(() {
+      return remoteDataSource.getAllPersons(page);
+    });
+  }
+
+  @override
+  Future<Either<Failure, List<PersonEntity>>> searchPerson(String query) async {
+    return await _getPersons(() {
+      return remoteDataSource.searchPerson(query);
+    });
+  }
+
+  Future<Either<Failure, List<PersonModel>>> _getPersons(
+      Future<List<PersonModel>> Function() getPersons) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remotePerson = await getPersons();
+        localDataSoutce.personsToCache(remotePerson);
+        return Right(remotePerson);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final LocationPerson = await localDataSoutce.getLastPersonsFromCache();
+        return Right(LocationPerson);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
+  }
+}
