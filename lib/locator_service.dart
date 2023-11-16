542d15b984c1b8548889d51039a9a286e6a43e50
Mishal12g <mishal12g@gmail.com>
2023-11-16 20:33:22 +0300

    feat: added dependency injection (service location)

---
+++ lib/locator_service.dart
@@ -0,0 +1,59 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:rick_and_morty/core/platform/network_info.dart';
+import 'package:rick_and_morty/feature/data/dataSources/person_local_data_cource.dart';
+import 'package:rick_and_morty/feature/data/dataSources/person_remote_data_source.dart';
+import 'package:rick_and_morty/feature/data/repositories/person_repository_impl.dart';
+import 'package:rick_and_morty/feature/domain/repositories/person_repository.dart';
+import 'package:rick_and_morty/feature/domain/usecases/get_all_persons.dart';
+import 'package:rick_and_morty/feature/domain/usecases/seatch_person.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/person_list_cubit/person_list_cubit.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/search_bloc/search_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // BLoC / Cubit
+  sl.registerFactory(
+    () => PersonListCubit(getAllPersons: sl()),
+  );
+  sl.registerFactory(
+    () => PersonSearchBloc(searchPersons: sl()),
+  );
+
+  // UseCases
+  sl.registerLazySingleton(() => GetAllPersons(sl()));
+  sl.registerLazySingleton(() => SearchPerson(sl()));
+
+  // Repository
+  sl.registerLazySingleton<PersonRepository>(
+    () => PersonRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSoutce: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<PersonRemoteDataSource>(
+    () => PersonRemoteDataSourceImpl(
+      client: http.Client(),
+    ),
+  );
+
+  sl.registerLazySingleton<PersonLocalDataSoutce>(
+    () => PersonLocalDataSoutceImpl(sharedPreferences: sl()),
+  );
+
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(sl()),
+  );
+
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/person_list_cubit/person_list_cubit.dart';
+import 'package:rick_and_morty/feature/presentation/bloc/search_bloc/search_bloc.dart';
+import 'package:rick_and_morty/locator_service.dart' as di;
 
-void main(List<String> args) {
+import 'feature/presentation/pages/person_screen.dart';
+import 'locator_service.dart';
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.amber,
-                child: Text("dsdasdas"),
-              ),
-            ],
-          ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PersonListCubit>(
+            create: (context) => sl<PersonListCubit>()),
+        BlocProvider<PersonSearchBloc>(
+            create: (context) => sl<PersonSearchBloc>()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          backgroundColor: Colors.black,
+          scaffoldBackgroundColor: Colors.grey,
         ),
+        home: const HomePage(),
       ),
     );
   }
